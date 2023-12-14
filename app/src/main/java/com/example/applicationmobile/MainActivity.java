e277ca8cdf529f6ccf0e250c8531654974d0f639
elite <faniriniainarakotomahenina@gmail.com>
2023-12-14 14:52:57 +0300

    Main

--- app/src/main/java/com/example/applicationmobile/MainActivity.java
+++ app/src/main/java/com/example/applicationmobile/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.applicationmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items.add("Matière");
+        items.add("Étudiant");
+        items.add("Classe");
+        items.add("Professeur");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView) findViewById(R.id.lvItems);
+        listView.setAdapter(itemsAdapter);
     }
 
 
